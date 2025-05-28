ae11d4297bff3d346aab47c86386de8a2896ff96
sadafskokota <4161hzpl6908@gmail.com>
2025-05-28 20:16:52 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+    @Override
+    String givenName() {
+        return "花子";
+    }
+
+    @Override
+    void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
